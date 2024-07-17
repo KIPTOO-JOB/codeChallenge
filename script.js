6994fdd8704debce5684761c258ebe926b4e5e58
CAPTAIN-JOB <kiptoojob73@gmail.com>
2024-07-17 17:39:07 +0300

    adding the buttton

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+	// const desc = document.querySelector("#desc");
 	const apiUrl = "https://db-json-pi.vercel.app/films";
 
 	fetch(apiUrl)
@@ -7,10 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
 			// console.log("Movies data:", movies);
 			const title = document.querySelector(".lists");
 			movies.map((movie) => {
+				console.log("Movie:", movie);
 				const list = document.createElement("li");
 				list.textContent = movie.title;
-				list.addEventListener("click", () => MovieDesc(movie.id));
+				list.addEventListener("click", () => {
+					MovieDesc(movie.id);
+					console.log("hello");
+				});
 				title.appendChild(list);
+
+				const movieDesc = document.createElement("img");
+				movieDesc.src = movie.poster;
+				desc.appendChild(movieDesc);
+
+				const movietitle = document.createElement("h2");
+				movietitle.textContent = movie.title;
+				desc.appendChild(movietitle);
 			});
 		});
 
@@ -19,6 +32,9 @@ document.addEventListener("DOMContentLoaded", () => {
 		fetch(movieUrl)
 			.then((res) => res.json())
 			.then((movie) => {
+				// const description = document.name(".description");
+
+				// Clear previous content
 				const description = document.querySelector(".description");
 				description.innerHTML = `
             <h1>${movie.title}</h1>
@@ -30,6 +46,16 @@ document.addEventListener("DOMContentLoaded", () => {
             <img src="${movie.poster}" alt="${movie.title}">
 
           `;
+				const button = document.querySelector("#buy-ticket");
+				button.addEventListener("click", () => {
+					if (movie.tickets_sold > 0) {
+						movie.tickets_sold--;
+						const ticketsSold = document.querySelector("#tickets-sold");
+						ticketsSold.textContent = `Tickets Sold: ${movie.tickets_sold}`;
+					} else {
+						alert("No more tickets available!");
+					}
+				});
 			});
 	}
 });
