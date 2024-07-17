4f2e8b82ac8a2aa52e14b01f3f5829e6e5f47f85
CAPTAIN-JOB <kiptoojob73@gmail.com>
2024-07-17 17:41:17 +0300

    fixing some bugs

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// const desc = document.querySelector("#desc");
 	const apiUrl = "https://db-json-pi.vercel.app/films";
 
 	fetch(apiUrl)
 		.then((res) => res.json())
 		.then((movies) => {
-			// console.log("Movies data:", movies);
 			const title = document.querySelector(".lists");
 			movies.map((movie) => {
-				console.log("Movie:", movie);
 				const list = document.createElement("li");
 				list.textContent = movie.title;
-				list.addEventListener("click", () => {
-					MovieDesc(movie.id);
-					console.log("hello");
-				});
+				list.addEventListener("click", () => MovieDesc(movie.id));
 				title.appendChild(list);
-
-				const movieDesc = document.createElement("img");
-				movieDesc.src = movie.poster;
-				desc.appendChild(movieDesc);
-
-				const movietitle = document.createElement("h2");
-				movietitle.textContent = movie.title;
-				desc.appendChild(movietitle);
 			});
 		});
 
@@ -32,20 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
 		fetch(movieUrl)
 			.then((res) => res.json())
 			.then((movie) => {
-				// const description = document.name(".description");
-
-				// Clear previous content
 				const description = document.querySelector(".description");
 				description.innerHTML = `
-            <h1>${movie.title}</h1>
-			<div>
-            <p>${movie.description}</p>
-			</div>
-			<h1> Movie Starts at : ${movie.showtime}</h1>
-			<h2>Tickets Sold: ${movie.tickets_sold}</h2>
-            <img src="${movie.poster}" alt="${movie.title}">
+					<h1>${movie.title}</h1>
+					<div>
+						<p>${movie.description}</p>
+					</div>
+					<h1>Movie Starts at: ${movie.showtime}</h1>
+					<h2 id="tickets-sold">Tickets Sold: ${movie.tickets_sold}</h2>
+					<img src="${movie.poster}" alt="${movie.title}">
+					<button id="buy-ticket">Buy Ticket</button>
+				`;
 
-          `;
+				// Add event listener to the button
 				const button = document.querySelector("#buy-ticket");
 				button.addEventListener("click", () => {
 					if (movie.tickets_sold > 0) {
